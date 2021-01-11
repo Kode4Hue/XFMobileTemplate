aa586aa385ef8a3c985141c192acf4e4427b4f0e
Hubert Graham <hubert.graham90@gmail.com>
2021-01-10 20:19:20 -0500

    ugraded xamarin forms shared project to c# 9

--- XfMobileTemplate/XfMobileTemplate.Ui/AssemblyInfo.cs
+++ XfMobileTemplate/XfMobileTemplate.Ui/AssemblyInfo.cs
@@ -1,3 +1,12 @@
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+/// <summary>
+/// Fix for record issue for using c#9
+/// Link: https://montemagno.com/enabling-c-9-in-xamarin-net-standard-projects/
+/// </summary>
+namespace System.Runtime.CompilerServices
+{
+    public class IsExternalInit { }
+}
