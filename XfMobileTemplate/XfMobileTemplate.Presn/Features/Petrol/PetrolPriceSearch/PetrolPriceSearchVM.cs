c4da5b13506a2c057147e8afa032b6fd85bfc5d6
Hubert Graham <hubert.graham90@gmail.com>
2021-01-10 23:52:07 -0500

    added initial view model for petrol search page

---
+++ XfMobileTemplate/XfMobileTemplate.Presn/Features/Petrol/PetrolPriceSearch/PetrolPriceSearchVM.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Prism.Navigation;
+using XfMobileTemplate.Presn.Features.Common.ViewModels;
+
+namespace XfMobileTemplate.Presn.Features.Petrol.PetrolPriceSearch
+{
+    public class PetrolPriceSearchVM: MediatRViewModelBase
+    {
+        public PetrolPriceSearchVM(INavigationService navigationService, IMediator mediator)
+            :base(navigationService, mediator)
+        {
+        }
+    }
+}
