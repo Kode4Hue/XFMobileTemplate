9ad9fa003e9e187637a9859dbf9bb2644b925b0b
Hubert Graham <hubert.graham90@gmail.com>
2021-01-11 02:10:20 -0500

    added validation exception and custom httpclient factory

---
+++ XfMobileTemplate.Ca.Application/Features/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace XfMobileTemplate.Ca.Application.Features.Common.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException() : base()
+        {
+        }
+
+        public NotFoundException(string message) : base(message)
+        {
+        }
+
+        public NotFoundException(string name, object key) : base($"Entity \"{name}\" ({key}) was not found.")
+        {
+        }
+    }
+}
---
+++ XfMobileTemplate.Ca.Application/Features/Common/Factories/ICustomHttpClientFactory.cs
@@ -0,0 +1,10 @@
+﻿using RestSharp;
+
+namespace XfMobileTemplate.Ca.Application.Features.Common.Factories
+{
+    public interface ICustomHttpClientFactory
+    {
+        IRestClient GetInstance();
+        void SetBaseAddress(string baseUrl);
+    }
+}
