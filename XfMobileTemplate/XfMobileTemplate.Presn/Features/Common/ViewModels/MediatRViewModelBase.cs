35e27a0c8a22a27ed826e09410608aa863b5dba1
Hubert Graham <hubert.graham90@gmail.com>
2021-01-10 23:37:52 -0500

    added mediatr view model base

---
+++ XfMobileTemplate/XfMobileTemplate.Presn/Features/Common/ViewModels/MediatRViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Prism.Navigation;
+
+namespace XfMobileTemplate.Presn.Features.Common.ViewModels
+{
+    public class MediatRViewModelBase: ViewModelBase
+    {
+        protected readonly IMediator Mediator;
+
+        public MediatRViewModelBase(INavigationService navigationService, IMediator mediator) : base(navigationService)
+        {
+            Mediator = mediator;
+        }
+    }
+}
