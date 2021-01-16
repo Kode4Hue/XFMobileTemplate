359434aeb2da2cf1e2ff9722cf7748533c4339d5
Hubert Graham <hubert.graham90@gmail.com>
2021-01-16 01:19:40 -0500

    added initial app settings example file

---
+++ XfMobileTemplate.Ca.Infrastructure/Features/Common/Constants/AppSettings.Example.cs
@@ -0,0 +1,10 @@
+﻿namespace XfMobileTemplate.Ca.Infrastructure.Features.Common.Constants
+{
+    public static class AppSettings
+    {
+        public const string Environment = AppEnvironment.Development;
+        public const bool EnableHttpProxy = false;
+        public const  string HttpProxyAddress = "192.168.0.15";
+        public const int HttpProxyPort = 5555;
+    }
+}
