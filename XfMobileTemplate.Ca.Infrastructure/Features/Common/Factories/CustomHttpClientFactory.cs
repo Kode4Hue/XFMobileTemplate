5f26d8b6c2075daf113df4b8b5d0892e6705219e
Hubert Graham <hubert.graham90@gmail.com>
2021-01-15 19:15:30 -0500

    added http client factory implementation

---
+++ XfMobileTemplate.Ca.Infrastructure/Features/Common/Factories/CustomHttpClientFactory.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.FluentAPI.Resolvers;
+using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Serializers.NewtonsoftJson;
+using System.Net;
+
+namespace XfMobileTemplate.Ca.Infrastructure.Features.Common.Factories
+{
+    public class CustomHttpClientFactory
+    {
+        public IRestClient GetInstance()
+        {
+            var httpClient = new RestClient()
+            {
+                ThrowOnAnyError = true
+            };
+
+            ConfigureHttpClientProxy(httpClient);
+            ConfigureSerializationSettings(httpClient);
+
+            return httpClient;
+
+
+        }
+
+        private void ConfigureHttpClientProxy(IRestClient httpClient)
+        {
+            if (httpClient != null)
+            {
+                var webProxy = new WebProxy("192.168.0.7", 5555);
+                httpClient.Proxy = webProxy;
+            }
+        }
+
+        private void ConfigureSerializationSettings(IRestClient httpClient)
+        {
+            if (httpClient != null)
+            {
+                httpClient.UseNewtonsoftJson(GenerateJsonSerializerConfigurations());
+            }
+        }
+
+        public JsonSerializerSettings GenerateJsonSerializerConfigurations()
+        {
+            return new JsonSerializerSettings
+            {
+                ContractResolver = BuildObjectsAndJsonContractMappings(),
+                NullValueHandling = NullValueHandling.Ignore
+            };
+        }
+
+        private FluentContractResolver BuildObjectsAndJsonContractMappings()
+        {
+            var jsonContractResolver = new FluentContractResolver();
+            //jsonContractResolver.AddConfiguration(new UserSignupMap());
+            return jsonContractResolver;
+        }
+
+    }
+}
