83ec89bd7a445e53e3c4edf267e5fa4b7999fff9
Hubert Graham <hubert.graham90@gmail.com>
2021-01-17 18:59:24 -0500

    enabled use of font awesome as font family

--- XfMobileTemplate/XfMobileTemplate.Presn/AssemblyInfo.cs
+++ XfMobileTemplate/XfMobileTemplate.Presn/AssemblyInfo.cs
@@ -1,6 +1,13 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+//Give the fonts a unique alias
+[assembly: ExportFont("font-awesome-pro-light.ttf", Alias = "fa-light")]
+[assembly: ExportFont("font-awesome-pro-regular.ttf", Alias = "fa-regular")]
+[assembly: ExportFont("font-awesome-pro-solid.ttf", Alias = "fa-solid")]
+[assembly: ExportFont("font-awesome-pro-brands.ttf", Alias = "fa-brands")]
 
 /// <summary>
 /// Fix for record issue for using c#9
